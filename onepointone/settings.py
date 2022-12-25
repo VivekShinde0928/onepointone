a9297cf81fbff74efe2ec6c17555e9c2dabb5bc8
VivekShinde0928 <65374853+VivekShinde0928@users.noreply.github.com>
2022-12-25 22:42:24 +0530

    Update settings.py
    changed db settings from localhost to 127.0.0.1

--- onepointone/settings.py
+++ onepointone/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'onepointonesolutions',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     },
 }
